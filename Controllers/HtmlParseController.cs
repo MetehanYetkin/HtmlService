4b1377f5e377003a2f1fd6e63bfa30e9717477f9
Lenovo <Lenovo@DESKTOP-2SDAI6C>
2022-03-27 22:01:31 +0300

    Add project files.

---
+++ Controllers/HtmlParseController.cs
@@ -0,0 +1,56 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HtmlService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HtmlParseController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetNodes(string url)
+        {
+          
+            var response = CallUrl(url).Result;
+            var linkList = ParseHtml(response);
+            //WriteToCsv(linkList);
+
+            return Ok(linkList);
+        }
+
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+
+        private List<string> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var programmerLinks = htmlDoc.DocumentNode.Descendants("li")
+                    .Where(node => !node.GetAttributeValue("class", "").Contains("tocsection")).ToList();
+
+            List<string> wikiLink = new List<string>();
+
+            foreach (var link in programmerLinks)
+            {
+                if (link.FirstChild.Attributes.Count > 0)
+                    wikiLink.Add(html + link.FirstChild.Attributes[0].Value);
+            }
+
+            return wikiLink;
+
+        }
+    }
+}
